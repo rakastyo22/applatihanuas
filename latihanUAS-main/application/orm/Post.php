bfc179cb7c958b62b6a6287de45e67e47e0a0552
rakastyo22 <rakastyo250rr@gmail.com>
2023-07-06 12:46:06 +0700

    rakauasselesai

---
+++ latihanUAS-main/application/orm/Post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Orm;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model {
+    protected $table = 'post';
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
